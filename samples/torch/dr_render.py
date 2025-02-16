2281ae8836518a62bb71c8e4b90f300669a5855d
mervekocabas <merve.kocabas@outlook.com>
2025-02-16 20:48:57 +0100

    img test

--- samples/torch/dr_render.py
+++ samples/torch/dr_render.py
@@ -199,19 +199,24 @@ class NVDRRenderer():
 if __name__ == "__main__":
     # 🔹 Load BEDLAM Data
     
-    bedlam_data = np.load("samples/data/bedlam_input/filtered_seq_000000.npz", allow_pickle=True)
-   
+    bedlam_data = np.load("samples/data/bedlam_input/filtered_first_image.npz", allow_pickle=True)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # 🔹 Extract necessary fields
     pose = torch.tensor(bedlam_data["pose_world"], dtype=torch.float32)  # Pose parameters
     shape = torch.tensor(bedlam_data["shape"], dtype=torch.float32)      # SMPL Shape
     cam_int = torch.tensor(bedlam_data["cam_int"], dtype=torch.float32)  # Intrinsics
     cam_ext = torch.tensor(bedlam_data["cam_ext"], dtype=torch.float32)  # Extrinsics
+    
+    pose = pose.to(device)
+    shape = shape.to(device)
+
     import ipdb; ipdb.set_trace()
     # 🔹 Initialize SMPL Model
-    smplx = SMPLX('samples/data/body_models/smplx/models/smplx/', gender='male').cuda()
+    smplx = SMPLX('samples/data/body_models/smplx/models/smplx/', gender='female').cuda()
+    smplx = smplx.to(device) 
 
     # 🔹 Get 3D vertices & faces from SMPL
-    smplx_output = smplx(body_pose=pose[:, 3:], global_orient=pose[:, :3], betas=shape)
+    smplx_output = smplx(body_pose=pose[:, 3:], global_orient=pose[:, :3], betas=shape,use_pca=False )
     vertices = smplx_output.vertices  # (B, N, 3)
     faces = smplx.faces_tensor.to(torch.int32)  # SMPL faces
 
