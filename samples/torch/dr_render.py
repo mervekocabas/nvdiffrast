456065ca7dbca43c63957d77c90773f45c719dfd
mervekocabas <merve.kocabas@outlook.com>
2025-02-18 18:55:50 +0100

    cam test

--- samples/torch/dr_render.py
+++ samples/torch/dr_render.py
@@ -378,6 +378,7 @@ if __name__ == "__main__":
     smplx_output = smplx(body_pose=pose[:, 3:66], global_orient=pose[:,:3], betas=shape[:, :10],use_pca=False )
     vertices = smplx_output.vertices  # (B, N, 3)
     cam_trans = cam_ext[:3, 3].to(device)
+    cam_trans[1] *= -1
     vertices = vertices + cam_trans[None, None]
     faces = smplx.faces_tensor.to(torch.int32)  # SMPL faces
     
